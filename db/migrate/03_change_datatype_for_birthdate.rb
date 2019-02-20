9e7b3d37de743b83617f0d0b57d5327fe7527dd8
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-20 20:48:21 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,3 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration
   def change
-    change_column :students, :birthdate, :birthday
+    change_column :students, :birthdate, :datetime
+  end
+end
